6e5e94ea0f0caa819f0eb479a47c5bc77fdeb8c6
Mikolaj <mikolaj.malarczyk@gmail.com>
2026-03-26 17:50:37 +0100

    First commit: Project init, Tool class created

---
+++ src/main/java/pl/cnc/manager/MainApp.java
@@ -0,0 +1,9 @@
+package pl.cnc.manager;
+
+public class MainApp
+{
+	public static void main(String[] args)
+	{
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/pl/cnc/manager/model/Tool.java
@@ -0,0 +1,13 @@
+package pl.cnc.manager.model;
+
+public class Tool {
+    private String id;
+    private String name;
+    private double diameter;
+
+    public Tool(String id, String name, double diameter) {
+        this.id = id;
+        this.name = name;
+        this.diameter = diameter;
+    }
+}
