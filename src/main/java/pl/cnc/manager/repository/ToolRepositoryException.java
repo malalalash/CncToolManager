884b6eebab91b54dbe6b4e00016b5d2de8b04f6e
Mikolaj <mikolaj.malarczyk@gmail.com>
2026-07-18 15:39:26 +0200

    Created custom ToolRepositoryException

---
+++ src/main/java/pl/cnc/manager/repository/ToolRepositoryException.java
@@ -0,0 +1,7 @@
+package pl.cnc.manager.repository;
+
+public class ToolRepositoryException extends RuntimeException {
+    public ToolRepositoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
