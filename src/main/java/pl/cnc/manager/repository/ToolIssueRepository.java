cd7257b5ba3b0a6e141cc733e3bea89241830e97
Mikolaj <mikolaj.malarczyk@gmail.com>
2026-07-23 12:05:18 +0200

    created ToolIssueService class

--- src/main/java/pl/cnc/manager/repository/ToolIssueRepository.java
+++ src/main/java/pl/cnc/manager/repository/ToolIssueRepository.java
@@ -1,6 +1,5 @@
 package pl.cnc.manager.repository;
 
-import pl.cnc.manager.model.Tool;
 import pl.cnc.manager.model.ToolIssue;
 
 import java.util.List;
---
+++ src/main/java/pl/cnc/manager/service/ToolIssueService.java
@@ -0,0 +1,20 @@
+package pl.cnc.manager.service;
+
+import pl.cnc.manager.model.ToolIssue;
+import pl.cnc.manager.repository.ToolIssueRepository;
+
+import java.util.List;
+
+
+public class ToolIssueService {
+
+    private final ToolIssueRepository repository;
+
+    public ToolIssueService (ToolIssueRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<ToolIssue> getHistory() {
+        return repository.findIssueHistory();
+    }
+}
